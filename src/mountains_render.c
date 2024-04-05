dc3f4c5adae10deee269cf1f13cdd79167dd956c
Jonathan Thomas <jonathan@opalise.co.uk>
2024-04-05 21:31:23 +0100

    faster mountain render

--- src/mountains_render.c
+++ src/mountains_render.c
@@ -12,36 +12,34 @@ void mountains_render()
     *((volatile int16_t *)BLITTER_ENDMASK_2) = -1;
     *((volatile int16_t *)BLITTER_ENDMASK_3) = -1;
     *((volatile int16_t *)BLITTER_SOURCE_X_INCREMENT) = 4;
-    *((volatile int16_t *)BLITTER_SOURCE_Y_INCREMENT) = -78; // originally 158
+    *((volatile int16_t *)BLITTER_SOURCE_Y_INCREMENT) = 80; // originally 158
     *((volatile int16_t *)BLITTER_DESTINATION_X_INCREMENT) = 8;
-    *((volatile int16_t *)BLITTER_DESTINATION_Y_INCREMENT) = -150;
+    *((volatile int16_t *)BLITTER_DESTINATION_Y_INCREMENT) = 8;
     *((volatile int16_t *)BLITTER_X_COUNT) = 20;
     *((volatile uint16_t *)BLITTER_HOP_OP) = 0x0203;
 
     uint16_t blitter_control_word;
     int32_t current_skew;
-    //int32_t skew_adjust;
-
     uint8_t *line_start_source = mountain_graphics;
     uint8_t *line_start_dest = (playfield->buffer) + 160*90;
 
     uint16_t scroll_pixels = mountains_shift >> 16;
-    //scroll_pixels = 62;
     current_skew = ((-scroll_pixels)-1);
     blitter_control_word = 0xc080 | (current_skew & 15);
 
     line_start_source += (scroll_pixels >> 2) & 0xfffffffc;
 
-    // 0-319 value = current_road_curvature >> 16;
+    *((volatile uint32_t *)BLITTER_SOURCE_ADDRESS) = line_start_source; // 8a32
+    *((volatile uint32_t *)BLITTER_DESTINATION_ADDRESS) = line_start_dest; // 8a32
+    *((volatile int16_t *)BLITTER_Y_COUNT) = 29; // 8a38
+    *((volatile uint16_t *)BLITTER_CONTROL) = blitter_control_word; // 8a3c
 
-    for (uint16_t index = 0; index < 29; index++) {
-        *((volatile uint32_t *)BLITTER_SOURCE_ADDRESS) = line_start_source; // 8a32
-        *((volatile uint32_t *)BLITTER_DESTINATION_ADDRESS) = line_start_dest; // 8a32
-        *((volatile int16_t *)BLITTER_Y_COUNT) = 2; // 8a38
-        *((volatile uint16_t *)BLITTER_CONTROL) = blitter_control_word; // 8a3c
+    line_start_source += 2;
+    line_start_dest += 2;
 
-        line_start_source += 160;
-        line_start_dest += 160;
-    }
+    *((volatile uint32_t *)BLITTER_SOURCE_ADDRESS) = line_start_source; // 8a32
+    *((volatile uint32_t *)BLITTER_DESTINATION_ADDRESS) = line_start_dest; // 8a32
+    *((volatile int16_t *)BLITTER_Y_COUNT) = 29; // 8a38
+    *((volatile uint16_t *)BLITTER_CONTROL) = blitter_control_word; // 8a3c
 }
 
