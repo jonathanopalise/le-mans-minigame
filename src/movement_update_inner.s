81e4cb6e2b4dbf7b9571e0721b6ef220afa1febc
Jonathan Thomas <jonathan@opalise.co.uk>
2024-08-29 15:10:38 +0100

    road movement optimise

--- src/movement_update_inner.s
+++ src/movement_update_inner.s
@@ -5,21 +5,19 @@
 
 _movement_update_inner_scenario_1:
 
-    move.l sp,a0
-
     ; should be in this order with sp going upwards:
     ;            sizeof(struct RoadScanline),                                        // needs to go into a2
     ;            &(current_road_scanline->current_logical_xpos),                     // needs to go into a0
     ;           &(current_road_scanline->logical_xpos_add_values[shift_required]   // needs to go into a1
 
-    move 6(a0),a1       ; sizeof(struct RoadScanline)
-    move.l 12(a0),d1    ; &(current_road_scanline->logical_xpos_add_values[shift_required]
-    move.l 8(a0),a0     ; &(current_road_scanline->current_logical_xpos)
+    move 6(sp),a1       ; sizeof(struct RoadScanline)
+    move.l 8(sp),a0     ; &(current_road_scanline->current_logical_xpos)
+    move.l 12(sp),d1    ; &(current_road_scanline->logical_xpos_add_values[shift_required]
     sub.l a0,d1
 
     rept 100
-    move.l 0(a0,d1),d0     ; current_road_scanline->logical_xpos_add_values[shift_required]
-    add.l d0,(a0)      ; current_logical_xpos+=(above value)
+    move.l 0(a0,d1),d0   ; current_road_scanline->logical_xpos_add_values[shift_required]
+    add.l d0,(a0)        ; current_logical_xpos+=(above value)
     add.l a1,a0          ; increment by sizeof(RoadScanline)
     endr
 
@@ -27,25 +25,16 @@ _movement_update_inner_scenario_1:
 
 _movement_update_inner_scenario_2:
 
-    move.l sp,a0
-    move.l a2,-(sp)
-
-    ; should be in this order with sp going upwards:
-    ;            sizeof(struct RoadScanline),                                        // needs to go into a2
-    ;            &(current_road_scanline->current_logical_xpos),                     // needs to go into a0
-    ;           &(current_road_scanline->logical_xpos_add_values[shift_required]   // needs to go into a1
-
-    move 6(a0),a2      ; sizeof(struct RoadScanline)
-    move.l 12(a0),a1
-    move.l 8(a0),a0
+    move 6(sp),a1       ; sizeof(struct RoadScanline)
+    move.l 8(sp),a0     ; &(current_road_scanline->current_logical_xpos)
+    move.l 12(sp),d1    ; &(current_road_scanline->logical_xpos_add_values[shift_required]
+    sub.l a0,d1
 
     rept 100
-    move.l (a1),d0     ; current_road_scanline->logical_xpos_add_values[shift_required]
-    sub.l d0,(a0)      ; current_logical_xpos+=(above value)
-    add.l a2,a0          ; increment by sizeof(RoadScanline)
-    add.l a2,a1          ; increment by sizeof(RoadScanline)
+    move.l 0(a0,d1),d0   ; current_road_scanline->logical_xpos_add_values[shift_required]
+    sub.l d0,(a0)        ; current_logical_xpos+=(above value)
+    add.l a1,a0          ; increment by sizeof(RoadScanline)
     endr
-    move.l (sp)+,a2
 
     rts
 
