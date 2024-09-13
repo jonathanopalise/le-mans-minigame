ffa8897881a8e46e6aed7e002ac771378984ca1f
Jonathan Thomas <jonathan@opalise.co.uk>
2024-09-13 11:32:42 +0100

    fix digital sound garbage at game start, add in lower volume tune from dma

--- src/mixer_init.s
+++ src/mixer_init.s
@@ -7,6 +7,15 @@ _mixer_init:
 	tst.w		hasDmaSound
 	beq			mixer_init_end
 
+    lea.l           bufferAudioMixer,a0
+    lea.l           addressAudioCurrentStart,a1
+    move.l          a0,(a1)+
+    lea.l           250(a0),a0
+    move.l          a0,(a1)+
+    move.l          a0,(a1)+
+    lea.l           250(a0),a0
+    move.l          a0,(a1)
+
 	movem.l		d0/a0-a1,-(sp)							; is this needed?
 
 	lea.l		$ffff8900.w,a0							; dma audio base address
@@ -24,30 +33,30 @@ _mixer_init:
     move.w		#%10101010100,d0						; left balance = +0dB
 	bsr			write_microwire
 
-	move.l		#dataSounds,addressAudioCurrentStart
-	lea.l		addressAudioCurrentStart,a1
+	;move.l		#dataSounds,addressAudioCurrentStart
+	;lea.l		addressAudioCurrentStart,a1
 
-	move.b		1(a1),$03(a0)							; set start address high byte
-	move.b		2(a1),$05(a0)							; set start address middle byte
-	move.b		3(a1),$07(a0)							; set start address low byte	
+	;move.b		1(a1),$03(a0)							; set start address high byte
+	;move.b		2(a1),$05(a0)							; set start address middle byte
+	;move.b		3(a1),$07(a0)							; set start address low byte	
 
-	add.l		#120704,(a1)							; size of sample
+	;add.l		#120704,(a1)							; size of sample
 
-	move.b		1(a1),$0f(a0)							; set end address high byte
-	move.b		2(a1),$11(a0)							; set end address middle byte
-	move.b		3(a1),$13(a0)							; set end address low byte
+	;move.b		1(a1),$0f(a0)							; set end address high byte
+	;move.b		2(a1),$11(a0)							; set end address middle byte
+	;move.b		3(a1),$13(a0)							; set end address low byte
 
-	move.b		#%10000001,$21(a0)						; set dma sound to mono 12517Hz
+	;move.b		#%10000001,$21(a0)						; set dma sound to mono 12517Hz
 
-	move.b		#1,$01(a0)								; start dma	
+	;move.b		#1,$01(a0)								; start dma	
 
-	lea.l		bufferAudioMixer,a0						; start address of audio mixer buffer
-	move.l		a0,(a1)+								; store start address of first half of buffer
-	lea.l		250(a0),a0								; add size of first half of buffer
-	move.l		a0,(a1)+								; store end address of first half of buffer				
-	move.l		a0,(a1)+								; store start address of second half of buffer
-	lea.l		250(a0),a0								; add size of second half of buffer
-	move.l		a0,(a1)									; store end address of second half of buffer
+	;lea.l		bufferAudioMixer,a0						; start address of audio mixer buffer
+	;move.l		a0,(a1)+								; store start address of first half of buffer
+	;lea.l		250(a0),a0								; add size of first half of buffer
+	;move.l		a0,(a1)+								; store end address of first half of buffer				
+	;move.l		a0,(a1)+								; store start address of second half of buffer
+	;lea.l		250(a0),a0								; add size of second half of buffer
+	;move.l		a0,(a1)									; store end address of second half of buffer
 
     movem.l		(sp)+,d0/a0-a1							; is this needed?
 
