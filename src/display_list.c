84d34624f40d22e808338cd7908f190d4327b5ff
Jonathan Thomas <jonathan@opalise.co.uk>
2024-09-30 20:40:57 +0100

    tidy up

--- src/display_list.c
+++ src/display_list.c
@@ -33,46 +33,10 @@ void display_list_add_sprite(struct SpriteDefinition *sprite_definition, int16_t
     num_visible_objects++;
 }
 
-int compare(const void *a, const void *b) {
-	const struct DisplayListItem *visible_object_1;
-	const struct DisplayListItem *visible_object_2;
-
-	visible_object_1=a;
-	visible_object_2=b;
-
-	return visible_object_1->ypos-visible_object_2->ypos;
-}
-
-void insertionSort(struct DisplayListItem arr[], int n)
-{
-    struct DisplayListItem key;
-    int i, j;
-    for (i = 1; i < n; i++) 
-    {
-        key = arr[i];
-        j = i - 1;
- 
-        /* Move elements of arr[0..i-1], 
-           that are greater than key, 
-           to one position ahead of 
-           their current position */
-        while (j >= 0 && arr[j].ypos < key.ypos) 
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-
 void display_list_execute() {
     struct DisplayListItem *current_display_list_item = next_free_display_list_item - 1;
 
-    //insertionSort(display_list, num_visible_objects);
     display_list_insertion_sort_fast(num_visible_objects);
-
-    //*((volatile uint16_t *)0xffff8240) = 0x222;
-    //*((volatile uint16_t *)0xffff8240) = 0x444;
 
     while (current_display_list_item >= display_list) {
         hardware_playfield_draw_sprite(
